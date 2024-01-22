41a5833553f76947a1866c427a91eea78867fb87
pengpeng <garenge@outlook.com>
2024-01-22 14:39:55 +0800

    tag 0.1.1, 更新readMe, 更新Example

--- Example/PPCustomAsyncOperation/PPViewController.m
+++ Example/PPCustomAsyncOperation/PPViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "PPViewController.h"
+#import <PPCustomAsyncOperation/PPCustomOperationQueue.h>
 
 @interface PPViewController ()
+
+@property (nonatomic, strong, nullable) PPCustomOperationQueue *queue;
 
 @end
 
@@ -16,8 +19,40 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];PPCustomOperationQueue *queue = [[PPCustomOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 1;
+
+    self.queue = queue;
+    __weak typeof(self) weakSelf = self;
+    self.queue.didFinishedOperationsBlock = ^(PPCustomOperationQueue * _Nonnull queue) {
+        weakSelf.queue = nil;
+    };
+}
+
+- (void)createOperations {
+    for (NSInteger index = 0; index < 20; index ++) {
+
+        PPCustomAsyncOperation *operation = [[PPCustomAsyncOperation alloc] init];
+        operation.identifier = [NSString stringWithFormat:@"ide_%ld", index];
+
+        operation.mainOperationDoBlock = ^BOOL(PPCustomAsyncOperation * _Nonnull operation) {
+
+            if (index % 2 == 0) {
+
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    NSLog(@"operation: %ld", index);
+                    [operation finishOperation];
+                });
+
+                return NO;
+            } else {
+                NSLog(@"operation: %ld", index);
+                return YES;
+            }
+        };
+
+        [self.queue addOperation:operation];
+    }
 }
 
 - (void)didReceiveMemoryWarning
--- README.md
+++ README.md
@@ -9,6 +9,42 @@
 
 To run the example project, clone the repo, and run `pod install` from the Example directory first.
 
+### 使用queue
+导入头文件`#import <PPCustomAsyncOperation/PPCustomOperationQueue.h>`
+### 只使用operation
+导入头文件`#import <PPCustomAsyncOperation/PPCustomAsyncOperation.h>`
+
+* 正常创建queue和operation
+* 同步的任务, 直接`operation.mainOperationDoBlock`中`return YES;`
+* 然后如果是异步的`operation`, 可以在`operation.mainOperationDoBlock`中返回NO, 并在合适的时候, 手动结束operation, 调用`[operation finishOperation];`
+* 具体的看`Example`
+
+```
+for (NSInteger index = 0; index < 20; index ++) {
+
+    PPCustomAsyncOperation *operation = [[PPCustomAsyncOperation alloc] init];
+    operation.identifier = [NSString stringWithFormat:@"ide_%ld", index];
+
+    operation.mainOperationDoBlock = ^BOOL(PPCustomAsyncOperation * _Nonnull operation) {
+
+        if (index % 2 == 0) {
+
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                NSLog(@"operation: %ld", index);
+                [operation finishOperation];
+            });
+
+            return NO;
+        } else {
+            NSLog(@"operation: %ld", index);
+            return YES;
+        }
+    };
+
+    [self.queue addOperation:operation];
+}
+```
+
 ## Requirements
 
 ## Installation
